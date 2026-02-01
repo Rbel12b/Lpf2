d7a8a8f951770f262a6227eb00899d68a2cb8756
Rbel12b <rbel12b@gmail.com>
2026-02-01 18:12:05 +0100

    Extend device lib

--- src/Lpf2DeviceDescLib.cpp
+++ src/Lpf2DeviceDescLib.cpp
@@ -298,4 +298,427 @@ namespace Lpf2DeviceDescriptors
             },
         }
     };
+    // Device 0x4C
+    const Lpf2DeviceDescriptor LPF2_DEVICE_TECHNIC_LARGE_ANGULAR_MOTOR_GREY =
+    {
+        .type = Lpf2DeviceType::TECHNIC_LARGE_ANGULAR_MOTOR_GREY,
+        .inModes = 0x0000,
+        .outModes = 0x0000,
+        .caps = 0x00,
+        .combos = { 0x0000 },
+        .modes =
+        {
+            {
+                "POWER",
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                "PCT",
+                0x00, 0x50,
+                1, DATA8, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x30 }}
+            },
+            {
+                "SPEED",
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                "PCT",
+                0x30, 0x70,
+                1, DATA8, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x21 }}
+            },
+            {
+                "POS",
+                -360.0f, 360.0f,
+                -100.0f, 100.0f,
+                -360.0f, 360.0f,
+                "DEG",
+                0x28, 0x68,
+                1, DATA32, 11, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x00, 0x00, 0x00, 0x24, 0x00, 0x00 }}
+            },
+            {
+                "APOS",
+                -180.0f, 179.0f,
+                -200.0f, 200.0f,
+                -180.0f, 179.0f,
+                "DEG",
+                0x32, 0x72,
+                1, DATA16, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x05, 0x00, 0x00, 0x00, 0x22, 0x00 }}
+            },
+            {
+                "CALIB",
+                0.0f, 3600.0f,
+                0.0f, 100.0f,
+                0.0f, 3600.0f,
+                "CAL",
+                0x00, 0x00,
+                2, DATA16, 5, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x40, 0x22 }}
+            },
+            {
+                "STATS",
+                0.0f, 65535.0f,
+                0.0f, 100.0f,
+                0.0f, 65535.0f,
+                "MIN",
+                0x00, 0x00,
+                14, DATA16, 5, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x00 }}
+            },
+        }
+    };
+
+    // Device 0x3E
+    const Lpf2DeviceDescriptor LPF2_DEVICE_TECHNIC_DISTANCE_SENSOR =
+    {
+        .type = Lpf2DeviceType::TECHNIC_DISTANCE_SENSOR,
+        .inModes = 0x0000,
+        .outModes = 0x0000,
+        .caps = 0x00,
+        .combos = { 0x0000 },
+        .modes =
+        {
+            {
+                "DISTL",
+                0.0f, 2500.0f,
+                0.0f, 100.0f,
+                0.0f, 250.0f,
+                "CM",
+                0x91, 0x00,
+                1, DATA16, 5, 1,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "DISTS",
+                0.0f, 320.0f,
+                0.0f, 100.0f,
+                0.0f, 32.0f,
+                "CM",
+                0xF1, 0x00,
+                1, DATA16, 4, 1,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "SINGL",
+                0.0f, 2500.0f,
+                0.0f, 100.0f,
+                0.0f, 250.0f,
+                "CM",
+                0x90, 0x00,
+                1, DATA16, 5, 1,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "LISTN",
+                0.0f, 1.0f,
+                0.0f, 100.0f,
+                0.0f, 1.0f,
+                "ST",
+                0x10, 0x00,
+                1, DATA8, 1, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "TRAW",
+                0.0f, 14577.0f,
+                0.0f, 100.0f,
+                0.0f, 14577.0f,
+                "uS",
+                0x90, 0x00,
+                1, DATA32, 5, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x00, 0x00, 0x00, 0x40, 0x00 }}
+            },
+            {
+                "LIGHT",
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                "PCT",
+                0x00, 0x10,
+                4, DATA8, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x20, 0x40 }}
+            },
+            {
+                "PING",
+                0.0f, 1.0f,
+                0.0f, 100.0f,
+                0.0f, 1.0f,
+                "PCT",
+                0x00, 0x90,
+                1, DATA8, 1, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x00, 0x00, 0x80, 0x40, 0x00 }}
+            },
+            {
+                "ADRAW",
+                0.0f, 1024.0f,
+                0.0f, 100.0f,
+                0.0f, 1024.0f,
+                "PCT",
+                0x90, 0x00,
+                1, DATA16, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "CALIB",
+                0.0f, 255.0f,
+                0.0f, 100.0f,
+                0.0f, 255.0f,
+                "PCT",
+                0x00, 0x00,
+                7, DATA8, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x40, 0x40 }}
+            },
+        }
+    };
+
+    // Device 0x4B
+    const Lpf2DeviceDescriptor LPF2_DEVICE_TECHNIC_MEDIUM_ANGULAR_MOTOR_GREY =
+    {
+        .type = Lpf2DeviceType::TECHNIC_MEDIUM_ANGULAR_MOTOR_GREY,
+        .inModes = 0x0000,
+        .outModes = 0x0000,
+        .caps = 0x00,
+        .combos = { 0x0000 },
+        .modes =
+        {
+            {
+                "POWER",
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                "PCT",
+                0x00, 0x50,
+                1, DATA8, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x30 }}
+            },
+            {
+                "SPEED",
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                -100.0f, 100.0f,
+                "PCT",
+                0x30, 0x70,
+                1, DATA8, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x21 }}
+            },
+            {
+                "POS",
+                -360.0f, 360.0f,
+                -100.0f, 100.0f,
+                -360.0f, 360.0f,
+                "DEG",
+                0x28, 0x68,
+                1, DATA32, 11, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x00, 0x00, 0x00, 0x24, 0x00, 0x00 }}
+            },
+            {
+                "APOS",
+                -180.0f, 179.0f,
+                -200.0f, 200.0f,
+                -180.0f, 179.0f,
+                "DEG",
+                0x32, 0x72,
+                1, DATA16, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x05, 0x00, 0x00, 0x00, 0x22, 0x00 }}
+            },
+            {
+                "CALIB",
+                0.0f, 3600.0f,
+                0.0f, 100.0f,
+                0.0f, 3600.0f,
+                "CAL",
+                0x00, 0x00,
+                2, DATA16, 5, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x40, 0x22 }}
+            },
+            {
+                "STATS",
+                0.0f, 65535.0f,
+                0.0f, 100.0f,
+                0.0f, 65535.0f,
+                "MIN",
+                0x00, 0x00,
+                14, DATA16, 5, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x00 }}
+            },
+        }
+    };
+
+    // Device 0x3D
+    const Lpf2DeviceDescriptor LPF2_DEVICE_TECHNIC_COLOR_SENSOR =
+    {
+        .type = Lpf2DeviceType::TECHNIC_COLOR_SENSOR,
+        .inModes = 0x0000,
+        .outModes = 0x0000,
+        .caps = 0x00,
+        .combos = { 0x0000 },
+        .modes =
+        {
+            {
+                "COLOR",
+                0.0f, 10.0f,
+                0.0f, 100.0f,
+                0.0f, 10.0f,
+                "IDX",
+                0xE4, 0x00,
+                1, DATA8, 2, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "REFLT",
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                "PCT",
+                0x30, 0x00,
+                1, DATA8, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "AMBI",
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                "PCT",
+                0x30, 0x00,
+                1, DATA8, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x00, 0x00, 0x00, 0x40, 0x00 }}
+            },
+            {
+                "LIGHT",
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                0.0f, 100.0f,
+                "PCT",
+                0x00, 0x10,
+                3, DATA8, 3, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x05, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "RREFL",
+                0.0f, 1024.0f,
+                0.0f, 100.0f,
+                0.0f, 1024.0f,
+                "RAW",
+                0x10, 0x00,
+                2, DATA16, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "RGB I",
+                0.0f, 1024.0f,
+                0.0f, 100.0f,
+                0.0f, 1024.0f,
+                "RAW",
+                0x10, 0x00,
+                4, DATA16, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "HSV",
+                0.0f, 360.0f,
+                0.0f, 100.0f,
+                0.0f, 360.0f,
+                "RAW",
+                0x10, 0x00,
+                3, DATA16, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x00, 0x00, 0x00, 0x40, 0x00, 0x00 }}
+            },
+            {
+                "SHSV",
+                0.0f, 360.0f,
+                0.0f, 100.0f,
+                0.0f, 360.0f,
+                "RAW",
+                0x10, 0x00,
+                4, DATA16, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x04, 0x00, 0x00, 0x00, 0x40, 0x00 }}
+            },
+            {
+                "DEBUG",
+                0.0f, 65535.0f,
+                0.0f, 100.0f,
+                0.0f, 65535.0f,
+                "RAW",
+                0x10, 0x00,
+                4, DATA16, 4, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x00, 0x40 }}
+            },
+            {
+                "CALIB",
+                0.0f, 65535.0f,
+                0.0f, 100.0f,
+                0.0f, 65535.0f,
+                "",
+                0x00, 0x00,
+                7, DATA16, 5, 0,
+                {},
+                0x00,
+                Lpf2Mode::Flags{{ 0x84, 0x04, 0x00, 0x00, 0x40, 0x40 }}
+            },
+        }
+    };
 }; // namespace Lpf2DeviceDescriptors
