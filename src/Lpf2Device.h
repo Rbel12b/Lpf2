e81a0aed121228fdcbb172554aa0a6bc4f68ce45
Rbel12b <rbel12b@gmail.com>
2026-02-01 17:57:43 +0100

    Add device descriptors, and their collection

--- src/Lpf2Device.h
+++ src/Lpf2Device.h
@@ -9,6 +9,16 @@
 
 using Lpf2DeviceCapabilityId = uint32_t;
 
+struct Lpf2DeviceDescriptor
+{
+    Lpf2DeviceType type;
+    uint16_t inModes;
+    uint16_t outModes;
+    uint8_t caps;
+    std::vector<uint16_t> combos;
+    std::vector<Lpf2Mode> modes;
+};
+
 class Lpf2Device
 {
 public:
