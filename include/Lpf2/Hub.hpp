5ba45f8b7bd2fe5363928eed6a070ebc7af302f7
Rbel12b <rbel12b@gmail.com>
2026-03-08 10:00:38 +0100

    Add copyright notices and licensing information to source files
    - Updated all relevant header files in the Lpf2 library to include the copyright notice for 2026 by Rbel12b.
    - Added GNU Affero General Public License information to ensure compliance with open-source licensing.
    - Ensured consistent documentation across all modified files for clarity and legal protection.

--- include/Lpf2/Hub.hpp
+++ include/Lpf2/Hub.hpp
@@ -1,10 +1,20 @@
-/*
- * Lpf2Hub.hpp - Arduino base class for controlling Powered UP and Boost controllers
+/**
+ *  Copyright (C) 2026 - Rbel12b
+ *  Copyright (C) 2020 - Cornelius Munz
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
- * (c) Copyright 2020 - Cornelius Munz
- * Released under MIT License
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
  *
- */
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  */
 
 #pragma once
 
